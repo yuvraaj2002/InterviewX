f00063c05c58c307ff4feaab42b951ddd1b86c88
Yuvraj Singh <ys2002github@gmail.com>
2024-03-13 19:40:47 +0530

    Made changes in the UI

--- streamlit/pages/6_Break_the_ICE.py
+++ streamlit/pages/6_Break_the_ICE.py
@@ -1,66 +1,126 @@
-import os
-import requests
 import streamlit as st
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.prompts import PromptTemplate
-import google.generativeai as genai
-from langchain.schema.output_parser import StrOutputParser
-from src.Tool import get_profile_url
-from src.Lookups import linkedin_lookup
+from typing import Tuple
+import streamlit as st
+from src.Likedin_agent import lookup as linkedin_lookup_agent
+from src.Scrapte_linkedin_data import scrape_linkedin_profile
+from src.Chains import (
+    get_summary_chain,
+    get_interests_chain,
+    get_ice_breaker_chain,
+)
+from src.Parsers import (
+    summary_parser,
+    topics_of_interest_parser,
+    ice_breaker_parser,
+    Summary,
+    IceBreaker,
+    TopicOfInterest,
+)
 
-
-@st.cache_resource
-def load_model():
-    llm = ChatGoogleGenerativeAI(
-        model="gemini-pro", google_api_key="AIzaSyBmdKcpl5PTuVBVsHsOdAlotEOOAInFxoU"
-    )
-    return llm
-
-
-def get_process_linkedin_data(linkedin_profile_url):
+st.markdown(
     """
-    Fetches and processes LinkedIn data using ProxyCurl API.
+        <style>
+               .block-container {
+                    padding-top: 0.0rem;
+                    padding-bottom: 0rem;
+                    # padding-left: 2rem;
+                    # padding-right:2rem;
+                }
+        </style>
+        """,
+    unsafe_allow_html=True,
+)
 
-    Args:
-        linkedin_profile_url (str): URL of the LinkedIn profile.
 
-    Returns:
-        dict: Processed LinkedIn data.
+def ice_break_with(name: str) -> Tuple[Summary, IceBreaker, TopicOfInterest, str]:
 
-    """
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
 
-    api_key = os.environ.get("PROXYCURL_API_KEY")
-    api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
-    headers = {"Authorization": f"Bearer {api_key}"}
-    params = {"url": linkedin_profile_url}
+    summary_chain = get_summary_chain()
+    summary_and_facts = summary_chain.run(information=linkedin_data)
+    summary_and_facts = summary_parser.parse(summary_and_facts)
 
-    # response = requests.get(api_endpoint, params=params, headers=headers)
-    response = requests.get(
-        "https://gist.githubusercontent.com/paul-villalobos/c59167135dea9081021d53b2faebad66/raw/88ec8ce557de26be827cd29a617dceee3eb9306d/eden-marco.json"
+    interests_chain = get_interests_chain()
+    interests = interests_chain.run(information=linkedin_data)
+    interests = topics_of_interest_parser.parse(interests)
+
+    ice_breaker_chain = get_ice_breaker_chain()
+    ice_breakers = ice_breaker_chain.run(information=linkedin_data)
+    ice_breakers = ice_breaker_parser.parse(ice_breakers)
+
+    return (
+        summary_and_facts,
+        interests,
+        ice_breakers,
+        linkedin_data.get("profile_pic_url"),
     )
-    data = response.json()
-
-    # Cleaning the data
-    cleaned_data = {
-        key: value
-        for key, value in data.items()
-        if value not in ([], "", None) and key != "people_also_viewed"
-    }
-    if cleaned_data.get("groups"):
-        for group_dict in cleaned_data.get("groups"):
-            group_dict.pop("profile_pic_url")
-
-    return cleaned_data
-
 
 
 def Ice_breaker_page():
-    st.title("Ice breaker")
-    person_name = st.text_input("Enter the name")
-    linkedin_url = linkedin_lookup(name=person_name)
-    st.write(linkedin_url)
-    # linkedin_data = get_process_linkedin_data("somehignsg")
-    # st.write(lookup(linkedin_data))
+    st.title("ICE breaker👋")
+    st.markdown(
+        "<p style='font-size: 20px; text-align: left;padding-right: 2rem;padding-bottom: 1rem;'>In preparation for an interview, it is advantageous to acquaint oneself with the background and interests of the interviewee. This facilitates effective communication and rapport-building during the interview process. To streamline this preparatory phase, a module has been developed to provide concise summaries, pertinent facts, and tailored icebreakers for the interviewee. By inputting the interviewee's name or relevant details, users can access a comprehensive overview to enhance their interaction and engagement during the interview.</p>",
+        unsafe_allow_html=True,
+    )
+    input_col, details_col = st.columns(spec=(1, 2), gap="large")
+
+    # Initialize content with default values
+    content = {"summary_and_facts": None, "interests": None, "ice_breakers": None, "profile_pic_url": None}
+
+    with input_col:
+        name = st.text_input("Enter the name of person")
+        info_bt = st.button("Extract information⛏️", use_container_width=True)
+        if name:
+            if info_bt:
+
+                # Assuming ice_break_with function retrieves information based on the name
+                summary_and_facts, interests, ice_breakers, profile_pic_url = (
+                    ice_break_with(name)
+                )
+                content = {
+                    "summary_and_facts": summary_and_facts.to_dict(),
+                    "interests": interests.to_dict(),
+                    "ice_breakers": ice_breakers.to_dict(),
+                    "profile_pic_url": profile_pic_url,
+                }
+        else:
+            st.error("Please provide a name to see get details")
+
+    with input_col:
+        if content['profile_pic_url'] is not None:
+            st.image(content['profile_pic_url'])
+
+    with details_col:
+        if name:
+            st.title("Meet " + name)
+            st.write(content)
+
+            # summary = content['summary_and_facts']["summary"]
+            # if summary is not None:
+            #     st.markdown(
+            #         f"<p style='text-align: left; font-size: 18px; '>{summary}</p>",
+            #         unsafe_allow_html=True,
+            #     )
+            # facts = content['summary_and_facts']["facts"]
+            # if facts is not None:
+            #     for fact in facts:
+            #         st.markdown(
+            #             f"<p style='text-align: left; font-size: 18px;'>➡️ {fact}</p>",
+            #             unsafe_allow_html=True,
+            #         )
+            #
+            # st.markdown(
+            #     "<h4 style='text-align: left;'>Break the Ice with,</h4>",
+            #     unsafe_allow_html=True,
+            # )
+            # ice_breakers = content['ice_breakers']["ice_breakers"]
+            # if ice_breakers is not None:
+            #     for ice_breaker in ice_breakers:
+            #         st.markdown(
+            #             f"<p style='text-align: left; font-size: 18px;background-color:#E0FFFF;padding:0.5rem;'>{ice_breaker}</p>",
+            #             unsafe_allow_html=True,
+            #         )
 
 
 Ice_breaker_page()
