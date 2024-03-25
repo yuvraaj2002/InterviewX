4622e04d02a457413a7051594f2e09c892fa6681
Yuvraj Singh <ys2002github@gmail.com>
2024-03-25 11:32:11 +0530

    Updated the path of the files

---
+++ pages/2_Resume_Radar.py
@@ -0,0 +1,212 @@
+import streamlit as st
+import base64
+from io import BytesIO
+from PyPDF2 import PdfReader
+from nltk.stem import WordNetLemmatizer
+import io
+import string
+import nltk
+import numpy as np
+from nltk.corpus import wordnet
+import re
+import gensim
+from gensim.models import Word2Vec, KeyedVectors
+from scipy.spatial.distance import cosine
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from langchain.schema.output_parser import StrOutputParser
+from langchain.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
+st.markdown(
+    """
+        <style>
+               .block-container {
+                    padding-top: 0.5rem;
+                    padding-bottom: 0rem;
+                    # padding-left: 2rem;
+                    # padding-right:2rem;
+                }
+        </style>
+        """,
+    unsafe_allow_html=True,
+)
+
+
+nltk.download("stopwords")
+import string
+
+nltk.download("punkt")
+punctuation = set(string.punctuation)
+stop_words = set(nltk.corpus.stopwords.words("english"))
+
+
+@st.cache_resource
+def load_model():
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-pro", google_api_key="AIzaSyBmdKcpl5PTuVBVsHsOdAlotEOOAInFxoU"
+    )
+    return llm
+
+
+def get_questions(text):
+
+    llm = load_model()
+    template = """
+            After reviewing the comprehensive details {resume_content} outlined in the candidate's resume, 
+            please provide five tailored questions that the candidate can anticipate, focusing specifically 
+            on their projects and experience. 
+            """
+
+    prompt = PromptTemplate(input_variables=["resume_content"], template=template)
+    output_parser = StrOutputParser()
+    chain = prompt | llm | output_parser
+
+    response = chain.invoke({"resume_content": text})
+    return response
+
+
+@st.cache_resource
+def load_w2v():
+    W2V_model = KeyedVectors.load_word2vec_format(
+        "artifacts/GoogleNews-vectors-negative300.bin.gz", binary=True, limit=500000
+    )
+    return W2V_model
+
+
+def clean_data(text):
+    """
+    This function preprocesses text by performing the following steps:
+
+    1. **Lowercasing:** Converts all characters to lowercase.
+    2. **Tokenization:** Splits the text into individual words using NLTK word tokenization.
+    3. **Stopword removal:** Removes common stop words (e.g., "the", "a", "is") from the word list.
+    4. **Punctuation removal:** Removes punctuation marks from the word list.
+    5. **Remove phone numbers and email addresses:** Removes phone numbers and email addresses from the text.
+    6. **Lemmatization:** Lemmatizes words to their base form.
+    7. **Rejoining:** Joins the remaining words back into a string with spaces in between.
+
+    Args:
+        text (str): The text to be cleaned.
+
+    Returns:
+        str: The cleaned text with stop words, punctuation, phone numbers, email addresses, and lemmatization applied.
+    """
+    # Lowercase the text
+    text_lower = text.lower()
+
+    # Tokenize the text
+    tokens = nltk.word_tokenize(text_lower)
+
+    # Remove punctuation marks
+    tokens = [word for word in tokens if word not in string.punctuation]
+
+    # Remove phone numbers
+    cleaned_tokens = []
+    for token in tokens:
+        if not re.match(r"\+?\d[\d -]{8,12}\d", token):
+            cleaned_tokens.append(token)
+
+    # Remove "gmail.com" after punctuation removal
+    final_tokens = []
+    for token in cleaned_tokens:
+        if not token.endswith("gmail.com"):
+            final_tokens.append(token)
+
+    # Lemmatization
+    lemmatizer = WordNetLemmatizer()
+    lemma_tokens = [lemmatizer.lemmatize(token, wordnet.VERB) for token in final_tokens]
+
+    # Rejoin the tokens into a string
+    cleaned_text = " ".join(lemma_tokens)
+    return cleaned_text
+
+
+# Define a function to get the vector representation of a document using Word2Vec
+def vectorize_text(doc, w2v_model):
+
+    # Remove out-of-vocabulary words and get Word2Vec vectors for the words in the document
+    words = [word for word in doc.split() if word in w2v_model]
+    if not words:
+        # If none of the words are in the Word2Vec model, return zeros
+        return np.zeros(300)
+
+    # Return the mean of Word2Vec vectors for words in the document
+    return np.mean(w2v_model[words], axis=0)
+
+
+def extract_clean_pdf(pdf_data):
+
+    file_object = io.BytesIO(pdf_data)  # Create a BytesIO object
+    reader = PdfReader(file_object)
+
+    # Extract text from all pages
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text()
+
+    return clean_data(text)
+
+
+def cosine_similarity(vec1, vec2):
+    """
+    Calculate the cosine similarity between two vectors.
+
+    Args:
+        vec1 (np.array): First vector.
+        vec2 (np.array): Second vector.
+
+    Returns:
+        float: The cosine similarity between the two vectors.
+    """
+    return 1 - cosine(vec1, vec2)
+
+
+def resume_radar_page():
+
+    col1, col2 = st.columns(spec=(2, 1.3), gap="large")
+    with col1:
+        st.markdown(
+            "<h1 style='text-align: left; font-size: 50px; '>Resume Radar👨‍💼</h1>",
+            unsafe_allow_html=True,
+        )
+        st.markdown(
+            "<p style='font-size: 18px; text-align: left;'>This module is dedicated solely to the refinement of your resume, the analysis of its alignment with a specific job description, and interview preparation. Through this process, we aim to ensure optimal coherence and relevance between your resume and the targeted job role, as well as equip you with the top 5 interview questions you can expect.</p>",
+            unsafe_allow_html=True,
+        )
+        job_description = st.text_input("Enter the job description")
+        uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+
+    with col2:
+
+        if uploaded_file is not None:
+            pdf_data = uploaded_file.read()
+            b64_pdf = base64.b64encode(pdf_data).decode("utf-8")
+            pdf_display = f'<embed src="data:application/pdf;base64,{b64_pdf}" width="690" height="740" type="application/pdf">'
+            st.markdown(pdf_display, unsafe_allow_html=True)
+
+            st.write("")
+            analyze_bt = st.button("Analyze my resume🔎", use_container_width=True)
+            if analyze_bt:
+                with col1:
+                    w2v_model = load_w2v()
+                    clean_text_resume = extract_clean_pdf(pdf_data)
+                    clean_text_jd = clean_data(job_description)
+
+                    resume_vector = vectorize_text(clean_text_resume, w2v_model)
+                    jd_vector = vectorize_text(clean_text_jd, w2v_model)
+                    similarity_score = np.round(
+                        cosine_similarity(resume_vector, jd_vector), 2
+                    )
+                    st.write(
+                        "<p style='font-size: 22px;text-align: center;background-color:#E0FFFF;'>Your resume and job description have <strong>"
+                        + str(similarity_score * 100)
+                        + "% similarity</strong></p>",
+                        unsafe_allow_html=True,
+                    )
+                    questions = get_questions(clean_text_resume)
+                    st.write(questions)
+
+
+resume_radar_page()
---
+++ pages/3_Posture_Analysis.py
@@ -0,0 +1,248 @@
+import cv2
+import mediapipe as mp
+import streamlit as st
+import tempfile
+import os
+import numpy as np
+import math
+import base64
+
+st.markdown(
+    """
+        <style>
+               .block-container {
+                    padding-top: 0.0rem;
+                    padding-bottom: 0rem;
+                    # padding-left: 2rem;
+                    # padding-right:2rem;
+                }
+        </style>
+        """,
+    unsafe_allow_html=True,
+)
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+
+def angle_between_points(p1, p2):
+    """
+    Calculate the angle between two points in 3D space.
+
+    Args:
+        p1 (Point): First point, with attributes x, y, and z representing its coordinates.
+        p2 (Point): Second point, with attributes x, y, and z representing its coordinates.
+
+    Returns:
+        float: Angle between the two points in degrees.
+
+    Notes:
+        - Requires the 'math' module.
+        - The function uses the dot product of the vectors formed by the two points
+          to calculate the angle between them.
+    """
+    x1, y1, z1 = p1.x, p1.y, p1.z
+    x2, y2, z2 = p2.x, p2.y, p2.z
+
+    # Calculate the dot product of the two vectors
+    dot_product = x1 * x2 + y1 * y2 + z1 * z2
+
+    # Calculate the magnitude of each vector
+    magnitude1 = math.sqrt(x1**2 + y1**2 + z1**2)
+    magnitude2 = math.sqrt(x2**2 + y2**2 + z2**2)
+
+    # Calculate the cosine of the angle between the vectors
+    cosine_angle = dot_product / (magnitude1 * magnitude2)
+
+    # Use arccos to get the angle in radians
+    angle_rad = math.acos(cosine_angle)
+
+    # Convert radians to degrees
+    angle_deg = math.degrees(angle_rad)
+
+    return angle_deg
+
+
+def process_video(
+    file_path, angles_shoulders, angles_lse, angles_rse, angles_lew, angles_rew
+):
+
+    # Decode video bytes into frames
+    cap = cv2.VideoCapture(file_path)
+
+    with mp_pose.Pose(
+        min_detection_confidence=0.8, min_tracking_confidence=0.8
+    ) as pose:
+        frame_count = 0
+        skip_count = 2  # Number of frames to skip between processing
+
+        # Create a placeholder for the image
+        image_placeholder = st.empty()
+
+        # Clear the lists before starting the loop
+        angles_shoulders.clear()
+        angles_lse.clear()
+        angles_rse.clear()
+        angles_lew.clear()
+        angles_rew.clear()
+
+        while cap.isOpened():
+            ret, frame = cap.read()
+
+            if not ret:
+                break
+
+            # Process only every skip_count frame
+            if frame_count % skip_count == 0:
+                # Recolor image to RGB
+                image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+                # Make detections
+                results = pose.process(image)
+
+                # Extract landmarks and render them (optional)
+                try:
+                    landmarks = results.pose_landmarks.landmark
+                    mp_drawing.draw_landmarks(
+                        image,
+                        results.pose_landmarks,
+                        mp_pose.POSE_CONNECTIONS,
+                        mp_drawing.DrawingSpec(
+                            color=(245, 117, 66), thickness=2, circle_radius=2
+                        ),
+                        mp_drawing.DrawingSpec(
+                            color=(245, 66, 230), thickness=2, circle_radius=2
+                        ),
+                    )
+                    # Retrieve left and right shoulder coordinates
+                    ls_cord = landmarks[
+                        mp.solutions.pose.PoseLandmark.LEFT_SHOULDER.value
+                    ]
+                    rs_cord = landmarks[
+                        mp.solutions.pose.PoseLandmark.RIGHT_SHOULDER.value
+                    ]
+
+                    le_cord = landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value]
+                    re_cord = landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value]
+
+                    lw_cord = landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value]
+                    rw_cord = landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value]
+
+                    angles_shoulders.append(angle_between_points(ls_cord, rs_cord))
+                    angles_lse.append(angle_between_points(ls_cord, le_cord))
+                    angles_rse.append(angle_between_points(rs_cord, re_cord))
+                    angles_lew.append(angle_between_points(lw_cord, re_cord))
+                    angles_rew.append(angle_between_points(rw_cord, re_cord))
+                except:
+                    pass
+
+                # Display the processed frame
+                image_placeholder.image(image, channels="RGB")
+
+            frame_count += 1
+
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+
+        # Release resources
+        cap.release()
+        cv2.destroyAllWindows()
+
+    return [angles_shoulders, angles_lse, angles_rse, angles_lew, angles_rew]
+
+
+def posture_analysis_page():
+    st.markdown(
+        "<h1 style='text-align: left; font-size: 58px;'>Posture analysis🕵️</h1>",
+        unsafe_allow_html=True,
+    )
+    st.markdown(
+        "<p style='font-size: 22px; text-align: left;padding-right: 2rem;padding-bottom: 1rem;'>In today's competitive job market, where first impressions matter immensely, it's crucial for candidates to convey confidence through their body language and posture during interviews. Recognizing the importance of this aspect, our module is designed to assist candidates in analyzing their posture while addressing interview questions.By simply uploading a brief 20-30 second video and initiating the analysis through the click of a button, our algorithm delves into a detailed examination of the upper body. Specifically, it meticulously scrutinizes the angles formed between the shoulders, elbows, and wrists.</p>",
+        unsafe_allow_html=True,
+    )
+
+    # Initialize lists to store left-shoulder, right-shoulder, shoulder, right-elbow-hand, left-elbow-hand
+    angles_lse = []
+    angles_rse = []
+    angles_shoulders = []
+    angles_rew = []
+    angles_lew = []
+
+    input_col, configuration_col = st.columns(spec=(2, 1.9), gap="large")
+    with input_col:
+        pass
+
+    temp_file_path = None  # Initialize temp_file_path outside the if block
+
+    with configuration_col:
+        video = st.file_uploader("Upload the video")
+        analyze_video = st.button("Analyze", use_container_width=True)
+        video_processed = False
+        if video is not None:
+            if analyze_video:
+                video_bytes = video.read()
+
+                # Save uploaded video to a temporary file
+                with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                    temp_file.write(video_bytes)
+                    temp_file_path = temp_file.name
+
+                with input_col:
+
+                    angles_shoulders, angles_lse, angles_rse, angles_lew, angles_rew = (
+                        process_video(
+                            temp_file_path,
+                            angles_shoulders,
+                            angles_lse,
+                            angles_rse,
+                            angles_lew,
+                            angles_rew,
+                        )
+                    )
+                    video_processed = True
+
+                # Remove the temporary file after processing if temp_file_path is defined
+                if temp_file_path:
+                    os.unlink(temp_file_path)
+        else:
+            st.error("Upload the video and then press analyze button❗")
+
+        # Convert lists to NumPy arrays
+        if video_processed:
+            angles_shoulders_array = np.array(angles_shoulders)
+            angles_lse_array = np.array(angles_lse)
+            angles_rse_array = np.array(angles_rse)
+            angles_lew_array = np.array(angles_lew)
+            angles_rew_array = np.array(angles_rew)
+
+            # Calculate the mean of each array
+            avg_angles = {
+                "Shoulders angle": angles_shoulders_array.mean(),
+                "Left shoulder-elbow angle": angles_lse_array.mean(),
+                "Right shoulder-elbow angle": angles_rse_array.mean(),
+                "Left elbow-wrist angle": angles_lew_array.mean(),
+                "Right elbow-wrist angle": angles_rew_array.mean(),
+            }
+
+            st.write("***")
+            row = st.columns(5)
+            index = 0
+            for name, avg_angle, col in zip(
+                avg_angles.keys(), avg_angles.values(), row
+            ):
+                tile = col.container(height=120)  # Adjust the height as needed
+                tile.markdown(
+                    f"<p style='text-align: left; font-size: 18px; '>Average {name}: <b>{avg_angle:.2f}</b></p>",
+                    unsafe_allow_html=True,
+                )
+                index += 1
+
+            st.write(
+                "<p style='font-size: 22px; text-align: center; background-color:#E0FFFF; margin-bottom: 2rem;'>Your resume and job description have <strong>"
+                + str(0.89 * 100)
+                + "% similarity</strong></p>",
+                unsafe_allow_html=True,
+            )
+
+
+posture_analysis_page()
